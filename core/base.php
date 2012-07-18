d94b451c959c094d2664d93d904429326084986b
Max <M1ks-47@yandex.ru>
2012-07-18 16:46:50 +0300

    добавил метод __call

--- core/base.php
+++ core/base.php
@@ -2,6 +2,29 @@
 
 class yBaseClass {
 	public $owner = NULL;
+        
+        function __call($func_name, $args) {
+
+                $vars_args = get_class_vars(__CLASS__);
+                $key_args = array_keys($vars_args);
+                $functionName = substr($func_name,0,3);
+                $propertyName = substr($func_name,3);
+                $propertyValue = $args[0];
+                
+                if(!in_array($propertyName, $key_args)){
+                    return NULL;
+                };
+                
+                switch ($functionName){
+                    case 'get':
+                        return $this->$propertyName;
+                    case 'set':
+                        $this->$propertyName = $propertyValue;
+                        return $this;
+                    default: 
+                        return NULL;
+                }
+        }
 	
 	public function setOwner($owner) {
 		$this->owner = $owner;
