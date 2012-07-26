477b749257b2525d91611564216a1bff6c264834
appelsin <rexe@ya.ru>
2012-07-26 14:02:26 +0400

    LGPL v.2.1 information added

--- core/template.php
+++ core/template.php
@@ -164,4 +164,23 @@ HEREDOC;
 	}
 }
 
+/*
+ * Copyright 2012 Roman Exempliarov. 
+ *
+ * This file is part of yCore framework.
+ *
+ * yCore is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * yCore is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with yCore. If not, see http://www.gnu.org/licenses/.
+ */
+
 ?>
--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 define ('_YEXEC', ''); //Первая строка каждого php-файла фреймворка должена быть "<?php defined ('_YEXEC')  or  die();"
 
 require_once('settings.php');
-require_once(ySettings::$corePath.'/factory.php');
+require_once(ySettings::$corePath.'/core.php');
 
 header('Content-type: text/html; charset=utf-8');
 
